66a1f2a1b94f1428f95f7a48ea2df0e2ba1bb601
medch24 <mohamed_sherif@alkawthar.edu.sa>
2025-09-28 19:10:14 +0300

    Update initial-data.js

--- api/initial-data.js
+++ api/initial-data.js
@@ -7,12 +7,9 @@ module.exports = async (req, res) => {
         await client.connect();
         const db = client.db('test');
         const collection = db.collection('plans');
-
         const teachers = await collection.distinct("Enseignant", { "Enseignant": { $ne: null, $ne: "" } });
         const classes = await collection.distinct("Classe", { "Classe": { $ne: null, $ne: "" } });
-
         res.status(200).json({ teachers, classes });
-
     } catch (error) {
         console.error("[initial-data] ERREUR:", error);
         res.status(500).json({ error: 'Erreur interne du serveur.', details: error.message });
